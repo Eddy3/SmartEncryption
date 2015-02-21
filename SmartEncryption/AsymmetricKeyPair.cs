abe794b3d30dfeea3e1246a5ade657249dded0f2
Graham Smith <gman98@me.com>
2015-02-21 15:11:23 -0600

    Added Asymmetric.cs and AsymmetricKeyPair.cs

---
+++ SmartEncryption/AsymmetricKeyPair.cs
@@ -0,0 +1,24 @@
+﻿using Sodium;
+
+namespace SmartEncryption
+{
+    public class AsymmetricKeyPair
+    {
+        private readonly Sodium.KeyPair _keyPair;
+
+        public byte[] PrivateKey
+        {
+            get { return _keyPair.PrivateKey; }
+        }
+
+        public byte[] PublicKey
+        {
+            get { return _keyPair.PublicKey; }
+        }
+
+        public AsymmetricKeyPair(Sodium.KeyPair keyPair)
+        {
+            _keyPair = keyPair;
+        }
+    }
+}
