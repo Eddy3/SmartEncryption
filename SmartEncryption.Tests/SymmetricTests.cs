444a95bced7078e6f98b09e3570d53ba413ae312
Adam Caudill <adam@adamcaudill.com>
2015-05-30 17:59:29 -0400

    Symmetric tests

---
+++ SmartEncryption.Tests/SymmetricTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartEncryption.Tests
+{
+    [TestClass]
+    public class SymmetricTests
+    {
+        [TestMethod]
+        public void BasicCanDecryptStringTest()
+        {
+            const string PLAINTEXT = "SmartEncryption Sym Test";
+            var key = Symmetric.GenerateKey();
+            var cipher = Symmetric.Encrypt(PLAINTEXT, key);
+            var plain = Symmetric.Decrypt(cipher, key);
+
+            Assert.AreEqual(PLAINTEXT, Encoding.UTF8.GetString(plain));
+        }
+    }
+}
