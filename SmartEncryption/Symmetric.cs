0eb61364e92b1fcd12778b4da905c465169590e8
Adam Caudill <adam@adamcaudill.com>
2015-05-30 17:59:06 -0400

    Cleanup constants

--- SmartEncryption/Symmetric.cs
+++ SmartEncryption/Symmetric.cs
@@ -2,18 +2,16 @@
 using System.Security.Cryptography;
 using Security.Cryptography;
 using System.IO;
+using System.Text;
 using Sodium;
 
 namespace SmartEncryption
 {
     public class Symmetric
     {
-        private const int KEY_SIZE = 256;
-        private const int KEY_SIZE_BYTES = 32;
-        private const int NONCE_SIZE = 96;
-        private const int NONCE_SIZE_BYTES = 12;
-        private const int TAG_SIZE = 128;
-        private const int TAG_SIZE_BYTES = 16;
+        private const int KEY_SIZE = 32;
+        private const int NONCE_SIZE = 12;
+        private const int TAG_SIZE = 16;
         private const byte VERSION = 0x01;
         private const int HEADER_LENGTH = 29;
 
@@ -21,7 +19,7 @@ namespace SmartEncryption
         {
             using (var rngCsp = new RNGCryptoServiceProvider())
             {
-                var key = new byte[KEY_SIZE_BYTES];
+                var key = new byte[KEY_SIZE];
                 rngCsp.GetBytes(key);
                 return key;
             }
@@ -30,6 +28,11 @@ namespace SmartEncryption
         public static byte[] DeriveKey(byte[] password, byte[] salt, PasswordHash.Strength strength = PasswordHash.Strength.Interactive)
         {
             return Hashing.DeriveKey(password, salt, strength);
+        }
+
+        public static byte[] Encrypt(string plaintext, byte[] key)
+        {
+            return Encrypt(Encoding.UTF8.GetBytes(plaintext), key);
         }
 
         public static byte[] Encrypt(byte[] plaintext, byte[] key)
@@ -57,12 +60,12 @@ namespace SmartEncryption
                     var record = new byte[HEADER_LENGTH + cipherText.Length];
                    
                     var offset = 1;
-                    Buffer.BlockCopy(nonce, 0, record, offset, NONCE_SIZE_BYTES);
+                    Buffer.BlockCopy(nonce, 0, record, offset, NONCE_SIZE);
 
-                    offset += NONCE_SIZE_BYTES;
-                    Buffer.BlockCopy(authenticationTag, 0, record, offset, TAG_SIZE_BYTES);
+                    offset += NONCE_SIZE;
+                    Buffer.BlockCopy(authenticationTag, 0, record, offset, TAG_SIZE);
 
-                    offset += TAG_SIZE_BYTES;
+                    offset += TAG_SIZE;
                     Buffer.BlockCopy(cipherText, 0, record, offset, cipherText.Length);
 
                     return record;
@@ -81,21 +84,21 @@ namespace SmartEncryption
             if (record[0] != VERSION)
             {
                 var offset = 1;
-                var nonce = new byte[NONCE_SIZE_BYTES];
-                Buffer.BlockCopy(record, 1, nonce, 0, NONCE_SIZE_BYTES);
+                var nonce = new byte[NONCE_SIZE];
+                Buffer.BlockCopy(record, 1, nonce, 0, NONCE_SIZE);
 
-                offset += NONCE_SIZE_BYTES;
-                var tag = new byte[TAG_SIZE_BYTES];
-                Buffer.BlockCopy(record, offset, tag, 0, TAG_SIZE_BYTES);
+                offset += NONCE_SIZE;
+                var tag = new byte[TAG_SIZE];
+                Buffer.BlockCopy(record, offset, tag, 0, TAG_SIZE);
 
-                offset += TAG_SIZE_BYTES;
+                offset += TAG_SIZE;
                 var cipherText = new byte[record.Length - HEADER_LENGTH];
                 Buffer.BlockCopy(record, offset, cipherText, 0, record.Length - HEADER_LENGTH);
 
                 using (var aes = new AuthenticatedAesCng())
                 {
                     aes.CngMode = CngChainingMode.Gcm;
-                    aes.KeySize = KEY_SIZE;
+                    aes.KeySize = KEY_SIZE * 8; //bytes to bits
                     aes.Key = key;
                     aes.IV = nonce;
                     aes.Tag = tag;
@@ -121,7 +124,7 @@ namespace SmartEncryption
         {
             using (var rngCsp = new RNGCryptoServiceProvider())
             {
-                var nonce = new byte[NONCE_SIZE_BYTES];
+                var nonce = new byte[NONCE_SIZE];
                 rngCsp.GetBytes(nonce);
                 return nonce;
             }
