6d6f5e42a1be8765835cee6b4d9b40cc74b1eb41
Adam Caudill <adam@adamcaudill.com>
2015-05-30 18:24:12 -0400

    Add Asymmetric tests

---
+++ SmartEncryption.Tests/AsymmetricTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartEncryption.Tests
+{
+    [TestClass]
+    public class AsymmetricTests
+    {
+        [TestMethod]
+        public void BasicCanDecryptTest()
+        {
+            const string PLAINTEXT = "This is a test of the SmartEncryption system...";
+            var senderKeys = Asymmetric.GenerateKeyPair();
+            var recipKeys = Asymmetric.GenerateKeyPair();
+
+            var cipher = Asymmetric.Encrypt(Encoding.UTF8.GetBytes(PLAINTEXT), senderKeys.PrivateKey,
+                recipKeys.PublicKey);
+            var plain = Asymmetric.Decrypt(cipher, recipKeys.PrivateKey, senderKeys.PublicKey);
+
+            Assert.AreEqual(PLAINTEXT, Encoding.UTF8.GetString(plain));
+        }
+    }
+}
