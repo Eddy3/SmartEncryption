dec12fc2844b92a428dfa154130e1118e9fff4a5
Adam Caudill <adam@adamcaudill.com>
2015-12-11 22:26:27 -0500

    Update docs and add overload

--- SmartEncryption.Tests/KeyDerivationTests.cs
+++ SmartEncryption.Tests/KeyDerivationTests.cs
@@ -8,6 +8,21 @@ namespace SmartEncryption.Tests
     public class KeyDerivationTests
     {
         [TestMethod]
+        public void DeriveKeyStringTest()
+        {
+            const string PASSWORD = "This is a test key";
+            const string SALT = "44071f6d181561670bda728d43fb79b443bb805afdebaf98622b5165e01b15fb";
+            const long OUTPUT_LENGTH = 32;
+
+            var hash1 = KeyDerivation.DeriveKey(PASSWORD, Utilities.HexToBinary(SALT));
+            var hash2 = KeyDerivation.DeriveKey(PASSWORD, Utilities.HexToBinary(SALT));
+
+            Assert.AreEqual(OUTPUT_LENGTH, hash1.Length);
+            Assert.AreEqual(OUTPUT_LENGTH, hash2.Length);
+            CollectionAssert.AreEqual(hash1, hash2);
+        }
+
+        [TestMethod]
         public void DeriveKeySimpleTest()
         {
             const string PASSWORD = "e125cee61c8cb7778d9e5ad0a6f5d978ce9f84de213a8556d9ffe202020ab4a6ed9074a4eb3416f9b168f137510f3a30b70b96cbfa219ff99f6c6eaffb15c06b60e00cc2890277f0fd3c622115772f7048adaebed86e";
--- SmartEncryption/KeyDerivation.cs
+++ SmartEncryption/KeyDerivation.cs
@@ -10,6 +10,23 @@ namespace SmartEncryption
     {
         private const int SALT_BYTES = 32;
 
+        /// <summary>
+        /// Creates a secure key based on a password.
+        /// </summary>
+        /// <param name="password">Password to be used to generate key.</param>
+        /// <param name="salt">Salt to be used; <see cref="GenerateSalt"/>.</param>
+        /// <returns>Secure derived key; 32 bytes.</returns>
+        public static byte[] DeriveKey(string password, byte[] salt)
+        {
+            return Sodium.PasswordHash.ScryptHashBinary(Encoding.UTF8.GetBytes(password), salt);
+        }
+
+        /// <summary>
+        /// Creates a secure key based on a password.
+        /// </summary>
+        /// <param name="password">Password to be used to generate key.</param>
+        /// <param name="salt">Salt to be used; <see cref="GenerateSalt"/>.</param>
+        /// <returns>Secure derived key; 32 bytes.</returns>
         public static byte[] DeriveKey(byte[] password, byte[] salt)
         {
             //hash the salt to ensure that it's the expected size of 32 bytes
@@ -18,6 +35,10 @@ namespace SmartEncryption
             return Sodium.PasswordHash.ScryptHashBinary(password, hashedSalt);
         }
 
+        /// <summary>
+        /// Generates a salt for use with <see cref="DeriveKey"/>
+        /// </summary>
+        /// <returns>32 random bytes</returns>
         public static byte[] GenerateSalt()
         {
             return Sodium.SodiumCore.GetRandomBytes(SALT_BYTES);
